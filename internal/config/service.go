253164d2a2ec5d8362949e86b79839e9b1736c7a
Matthew Tanudjaja <mrexmelle@gmail.com>
2024-01-13 01:49:57 +0700

    ORGS-5 Convert configuration from Neo4j to PostgreSQL (#6)
    * ORGS-5 Convert configuration

--- internal/config/service.go
+++ internal/config/service.go
@@ -1,14 +1,46 @@
 package config
 
+import (
+	"strings"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
 type Service struct {
 	ConfigRepository Repository
+	ReadDb           *gorm.DB
+	WriteDb          *gorm.DB
 }
 
 func NewService(
 	cr Repository,
 ) *Service {
+	readDb, err := gorm.Open(
+		postgres.Open(strings.TrimSpace(cr.GetReadDsn())),
+		&gorm.Config{
+			Logger: logger.Default.LogMode(logger.Info),
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	writeDb, err := gorm.Open(
+		postgres.Open(strings.TrimSpace(cr.GetWriteDsn())),
+		&gorm.Config{
+			Logger: logger.Default.LogMode(logger.Info),
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
 	return &Service{
 		ConfigRepository: cr,
+		ReadDb:           readDb,
+		WriteDb:          writeDb,
 	}
 }
 
