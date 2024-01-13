25c7841ef78aac5b4693445739e3f294cc6eb980
Matthew Tanudjaja <mrexmelle@gmail.com>
2024-01-13 08:55:28 +0700

    ORGS-5 Add documentation (#11)

--- cmd/main.go
+++ cmd/main.go
@@ -14,7 +14,7 @@ import (
 // @license.name  Apache 2.0
 // @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host      localhost:8080
+// @host      localhost:8081
 // @BasePath  /
 
 // @externalDocs.description  OpenAPI
