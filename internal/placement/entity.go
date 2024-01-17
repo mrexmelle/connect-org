8b5fd60d54225475e2c001a93c0176da28689096
Matthew Tanudjaja <mrexmelle@gmail.com>
2024-01-18 01:22:51 +0700

    ORGS-17 Introduce placement package (#19)

---
+++ internal/placement/entity.go
@@ -0,0 +1,8 @@
+package placement
+
+type Entity struct {
+	Id             string `json:"id"`
+	OrganizationId string `json:"organization_id"`
+	RoleId         string `json:"role_id"`
+	Ehid           string `json:"ehid"`
+}
