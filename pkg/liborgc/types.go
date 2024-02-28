e4d70e12092e9ef1162500962612faee90a12cc2
matthew-r-tanudjaja <matthew.r.tanudjaja@linecorp.com>
2024-02-28 23:37:35 +0700

    ORG-40 Add missing datatype

--- pkg/liborgc/types.go
+++ pkg/liborgc/types.go
@@ -3,7 +3,9 @@ package liborgc
 import (
 	"github.com/mrexmelle/connect-org/internal/dto"
 	"github.com/mrexmelle/connect-org/internal/member"
+	"github.com/mrexmelle/connect-org/internal/membership"
 )
 
 type GetMembershipHistoryResponseDto = member.GetHistoryResponseDto
+type MembershipViewEntity = membership.ViewEntity
 type ServiceError = dto.ServiceError
