f6e12e3dbbbf72ec006679fc9bf2679602ee0b50
matthew-r-tanudjaja <matthew.r.tanudjaja@linecorp.com>
2024-02-28 18:45:45 +0700

    ORG-40 Add SDK

---
+++ pkg/liborgc/types.go
@@ -0,0 +1,9 @@
+package liborgc
+
+import (
+	"github.com/mrexmelle/connect-org/internal/dto"
+	"github.com/mrexmelle/connect-org/internal/member"
+)
+
+type GetMembershipHistoryResponseDto = member.GetHistoryResponseDto
+type ServiceError = dto.ServiceError
